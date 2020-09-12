c7160927f4c9ad6cec7f59f18555d878986d39b8
sunjay_local <65554107+sarmstead@users.noreply.github.com>
2020-09-12 18:11:49 -0400

    Configure /api/quotes route

--- api-router.js
+++ api-router.js
@@ -13,5 +13,58 @@ apiRouter.get('/quotes/random', (req, res, next) => {
     res.send(randomQuote);
 });
 
+apiRouter.get('/quotes', (req, res, next) => {
+    // searchTerms checks the length of the keys inside of req.query. Zero length means there are no query params.
+    const searchTerms = () => {
+        const query = req.query;
+        return Object.keys(query).length;
+    };
+    
+    const personID = req.query.person;
+    
+    // personIndex loops through every index in the quotes array. If req.query.person matches quotes[i]['person'], that's put into a temporary array.
+    // If req.query.person does not match quotes[i]['person'] the temporary array length will be zero. Zero length means the search query does not exist in our records.
+    const  personIndex = () => {
+        let arr = []
+        for (let i = 0; i < quotes.length; i++) {
+            if (quotes[i]['person'] === personID) {
+                arr.push(i);
+            }
+        }
+        if (arr.length === 0) {
+            return false;
+        } else if (arr.length > 0) {
+            return true;
+        }
+    };
+    const arrayQuotes = () => {
+        const obj = {
+            quotes: quotes
+        };
+        return obj; 
+    }
+    const returnAllPerson = () => {
+        let arr = []
+        for (let i = 0; i < quotes.length; i++) {
+            if (quotes[i]['person'] == personID) {
+                arr.push({
+                    quote: quotes[i]['quote'],
+                    person: quotes[i]['person']
+                }) 
+            }
+        }
+        return arr;
+    }
+    
+    if (searchTerms() === 0) {
+        res.send(arrayQuotes());
+    } else if (personIndex()) {
+        res.send(returnAllPerson());
+    } else if (!personIndex()) {
+        res.send([]);
+    }
+
+
+});
 
 module.exports = apiRouter;
